44721600ba20f5bb39c38dc4586d7bd52ea0d8bd
Choi.SY <sollly0829@gmail.com>
2021-09-07 14:37:29 +0900

    20210907 fx06_team project 기본틀 생성

---
+++ src/fx06_team/common/alert/AlertClass.java
@@ -0,0 +1,14 @@
+package fx06_team.common.alert;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class AlertClass {
+
+	public static void alert(String msg) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setContentText(msg);
+		alert.show();
+	}
+	
+}
---
+++ src/fx06_team/main/Controller.java
@@ -0,0 +1,45 @@
+package fx06_team.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import fx06_team.common.db.DBClass;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+public class Controller implements Initializable {
+
+	Parent root;
+	DBClass db;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		db = new DBClass();
+		
+	}
+
+	public void setRoot(Parent root) {
+		this.root = root;
+	}
+	
+	public void mail() {
+		System.out.println("메일선택");
+	}
+	
+	public void blog() {
+		
+	}
+	
+	public void cafe() {
+		
+	}
+	
+	public void kin() {
+		
+	}
+	
+	public void shopping() {
+		
+	}
+	
+}
